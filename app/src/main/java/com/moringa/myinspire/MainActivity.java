967a2eee5c9448cdf1f04fbc9df1fa08adf358fa
danielsind <danielsindet@gmail.com>
2020-03-03 11:06:45 +0300

    Added new intent view from Main Activity to QuotesHome Activity through button id login

--- app/src/main/java/com/moringa/myinspire/MainActivity.java
+++ app/src/main/java/com/moringa/myinspire/MainActivity.java
@@ -2,13 +2,29 @@ package com.moringa.myinspire;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
-
+@BindView(R.id.login)
+    Button mLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this,QuotesHome.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
