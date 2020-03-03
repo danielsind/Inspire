19b67a396f4a0231030c8ba0c17b1e20d9fdbd0d
danielsind <danielsindet@gmail.com>
2020-03-03 10:51:28 +0300

    Override all the methods in ViewPagerAdapter that belong to FragmentPagerAdapter class

--- app/src/main/java/com/moringa/myinspire/ViewPagerAdapter.java
+++ app/src/main/java/com/moringa/myinspire/ViewPagerAdapter.java
@@ -1,4 +1,37 @@
 package com.moringa.myinspire;
 
-public class ViewPagerAdapter {
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    public  final List<Fragment> fragmentList = new ArrayList<>();
+    public final List<String> fragmentTitle = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager fm){
+        super(fm);
+    }
+    @Override
+    public Fragment getItem(int position){
+        return fragmentList.get(position);
+    }
+    @Override
+    public int getCount(){
+        return fragmentList.size();
+    }
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return fragmentTitle.get(position);
+    }
+    public void addFragment(Fragment fragment, String title){
+        fragmentList.add(fragment);
+        fragmentTitle.add(title);
+    }
 }
