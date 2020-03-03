41349cfedf2d8bc759f56503b7ed6d41768bea03
danielsind <danielsindet@gmail.com>
2020-03-03 10:39:38 +0300

    Added the BInding View Insantiation to Activity QuotesHome.java

--- app/src/main/java/com/moringa/myinspire/QuotesHome.java
+++ app/src/main/java/com/moringa/myinspire/QuotesHome.java
@@ -1,7 +1,14 @@
 package com.moringa.myinspire;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
+
+import butterknife.BindView;
 
 public class QuotesHome extends AppCompatActivity {
+    @BindView(R.id.tabLayout) TabLayout tabLayout;
+    @BindView(R.id.viewPager) ViewPager viewPager;
 
 }
