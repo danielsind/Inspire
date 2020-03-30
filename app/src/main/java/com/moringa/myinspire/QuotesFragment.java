2ae39e932a66911f8842de8d53694ecb43a1321d
danielsind <danielsindet@gmail.com>
2020-03-30 19:26:15 +0300

    Created an xml file to be dispalyed by th adapter --

--- app/src/main/java/com/moringa/myinspire/QuotesFragment.java
+++ app/src/main/java/com/moringa/myinspire/QuotesFragment.java
@@ -1,28 +1,77 @@
 package com.moringa.myinspire;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ListView;
-
-import com.moringa.myinspire.adapter.MyArrayAdapter;
-import com.moringa.myinspire.model.Quote;
-import com.moringa.myinspire.network.GetQuote;
+import com.moringa.myinspire.adapter.QuotesRecyclerViewAdapter;
+import com.moringa.myinspire.model.QuoteResponse;
+import com.moringa.myinspire.network.GetQuoteInterface;
 import com.moringa.myinspire.network.RetrofitClient;
 
 import java.util.List;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class QuotesFragment extends Fragment{
+    @BindView(R.id.recyclerView)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.authorTextView)
+    TextView mAuthorTextView;
+    @BindView(R.id.quoteTextView) TextView mQuoteTextView;
+    private List<QuoteResponse> mQuotes;
+    private QuotesRecyclerViewAdapter mQuoteAdapter;
+
+    public QuotesFragment(){
+
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_quotes, container, false);
+        ButterKnife.bind(this, view);
+
+
+
+        GetQuoteInterface client = RetrofitClient.getRetrofit();
+        Call<List<QuoteResponse>> call = client.getQuotes();
+
+        call.enqueue(new Callback<List<QuoteResponse>>() {
+            @Override
+            public void onResponse(Call<List<QuoteResponse>> call, Response<List<QuoteResponse>> response) {
+                if (response.isSuccessful()){
+                    mQuotes = response.body();
+                    RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+                    mQuoteAdapter = new QuotesRecyclerViewAdapter(getContext(), mQuotes);
+                    mRecyclerView.setLayoutManager(layoutManager);
+                    mRecyclerView.setAdapter(mQuoteAdapter);
+                }
+                else{
+
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<QuoteResponse>> call, Throwable t) {
+
+            }
+        });
+
+        return view;
+    }
+
 
 }
--- app/src/main/java/com/moringa/myinspire/network/GetQuoteInterface.java
+++ app/src/main/java/com/moringa/myinspire/network/GetQuoteInterface.java
@@ -1,14 +1,13 @@
 package com.moringa.myinspire.network;
 
-import com.moringa.myinspire.model.Quote;
+import com.moringa.myinspire.model.QuoteResponse;
 
 import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
-import retrofit2.http.Query;
 
 public interface GetQuoteInterface  {
     @GET
-    Call<List<Quote>> getQuote(@Query("format") String json);
+    Call<List<QuoteResponse>> getQuotes();
 }
