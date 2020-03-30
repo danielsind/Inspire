2da0176e23fada59a5783c7b708233b8bfed5014
danielsind <danielsindet@gmail.com>
2020-03-30 19:24:19 +0300

    Changed the REtrofit Client

--- app/src/main/java/com/moringa/myinspire/network/RetrofitClient.java
+++ app/src/main/java/com/moringa/myinspire/network/RetrofitClient.java
@@ -4,17 +4,17 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static Retrofit retrofit;
-    private static String BASE_URL = "http://ourquotesapp.herokuapp.com/?format=json";
+    private static Retrofit retrofit = null;
+    private static String BASE_URL = "http://ourquotesapp.herokuapp.com?format=json";
 
-    public static Retrofit  getRetrofit(){
-        if (retrofit == null){
+    public static GetQuoteInterface  getRetrofit(){
+        if (retrofit != null){
             retrofit =  new Retrofit
                     .Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
-        return retrofit;
+        return retrofit.create(GetQuoteInterface.class);
     }
 }
