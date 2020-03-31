066469786791ad650834d4f920339a4013f2abee
danielsind <danielsindet@gmail.com>
2020-03-31 16:00:16 +0300

    Changed the endpoint /quotes

--- app/src/main/java/com/moringa/myinspire/network/GetQuoteInterface.java
+++ app/src/main/java/com/moringa/myinspire/network/GetQuoteInterface.java
@@ -1,6 +1,7 @@
 package com.moringa.myinspire.network;
 
 import com.moringa.myinspire.model.QuoteResponse;
+import com.moringa.myinspire.model.QuotesApiResponse;
 
 import java.util.List;
 
@@ -8,6 +9,6 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 
 public interface GetQuoteInterface  {
-    @GET
-    Call<List<QuoteResponse>> getQuotes();
+    @GET("/quotes")
+    Call<List<QuotesApiResponse>> getQuotes();
 }
