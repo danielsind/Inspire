ec7d5e970e3b0aa88fa11ffe9a951641629f055c
danielsind <danielsindet@gmail.com>
2020-03-31 15:59:36 +0300

    Changed the BASE_URL api

--- app/src/main/java/com/moringa/myinspire/network/RetrofitClient.java
+++ app/src/main/java/com/moringa/myinspire/network/RetrofitClient.java
@@ -4,17 +4,18 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static Retrofit retrofit = null;
-    private static String BASE_URL = "http://ourquotesapp.herokuapp.com?format=json";
+    private static Retrofit retrofit;
+    private static String BASE_URL = "https://programming-quotes-api.herokuapp.com";
 
-    public static GetQuoteInterface  getRetrofit(){
-        if (retrofit != null){
+    public static GetQuoteInterface getRetrofit(){
+        if (retrofit == null){
             retrofit =  new Retrofit
                     .Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
+        assert retrofit != null;
         return retrofit.create(GetQuoteInterface.class);
     }
 }
