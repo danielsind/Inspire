f704c965c7e5a02fe36c02af36687ab6319450d6
danielsind <danielsindet@gmail.com>
2020-04-02 12:47:17 +0300

    Added an image and text view on the profile fragment

--- app/src/main/java/com/moringa/myinspire/fragments/ProfileFragment.java
+++ app/src/main/java/com/moringa/myinspire/fragments/ProfileFragment.java
@@ -19,27 +19,28 @@ import butterknife.BindView;
 
 
 public class ProfileFragment extends Fragment {
-    ListView lv;
-    SearchView searchView;
-    MyArrayAdapter adapter;
-    @BindView(R.id.shareButton)
-    FloatingActionButton mShareButton;
 
-    public String[] posts = new String[] {"Work Hard Always","Be Positive", "Never give Up","Genius is HardWork","Winners win from the start"};
-    public String[] author = new String[] {"James","Myles","Jonah","Henry","Timothy"};
 
-    public ProfileFragment (){
-
-    }
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_profile, container, false);
-        lv = (ListView) view.findViewById(R.id.listView);
-        adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_1, posts, author);
-        lv.setAdapter(adapter);
-        return view;
-    }
 }
-
+//    ListView lv;
+//    SearchView searchView;
+//    MyArrayAdapter adapter;
+//    @BindView(R.id.shareButton)
+//    FloatingActionButton mShareButton;
+//
+//    public String[] posts = new String[] {"Work Hard Always","Be Positive", "Never give Up","Genius is HardWork","Winners win from the start"};
+//    public String[] author = new String[] {"James","Myles","Jonah","Henry","Timothy"};
+//
+//    public ProfileFragment (){
+//
+//    }
+//
+//    @Nullable
+//    @Override
+//    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+//        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+//        lv = (ListView) view.findViewById(R.id.listView);
+//        adapter = new MyArrayAdapter(this, android.R.layout.simple_list_item_1, posts, author);
+//        lv.setAdapter(adapter);
+//        return view;
+//    }
